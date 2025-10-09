b0af899416d5764f0e44522007716c2400f24ff4
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-10-08 22:09:03 -0700

    Made ultra berries give you +5 score instead of +1
    progress ahahaha (making the actual boost is gonna be so fun trust)

--- Scripts/BerryLogic.cs
+++ Scripts/BerryLogic.cs
@@ -2,10 +2,22 @@ using Godot;
 using System;
 
 public partial class BerryLogic : Node2D {
+    string BerryType = "";
+
     private void _on_hitbox_area_entered(Area2D area) {
-        Globals.Score += 1;
+        Texture2D Sprite = GetNode<Sprite2D>("Sprite").Texture;
+
+        string BerryTexturesFolder = "res://Textures/Berries/";
+        if (Sprite == GD.Load(BerryTexturesFolder + "Berry.png")) {
+            BerryType = "normal";
+        } else if (Sprite == GD.Load(BerryTexturesFolder + "UltraBerry.png")) {
+            BerryType = "ultra";
+        }
+
+        int ScoreAdder = (BerryType == "normal") ? 1 : 5;
+        Globals.Score += ScoreAdder;
         if (Globals.Score > Globals.HighScore) {
-            Globals.HighScore += 1;
+            Globals.HighScore += ScoreAdder;
         }
 
         QueueFree();
