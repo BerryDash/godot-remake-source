213dd83e24a0c44cf3b3f3140bd3965346d5ef53
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-10-08 23:06:56 -0700

    Added a label for the effect timer
    It looks a bit weird but I couldn't be bothered to fix it

--- Scripts/ScoreText.cs
+++ Scripts/ScoreText.cs
@@ -5,8 +5,19 @@ public partial class ScoreText : Control {
     public override void _Process(double delta) {
         Label ScoreText = GetNode<Label>("ScoreLabel");
         Label HighScoreText = GetNode<Label>("HighScoreLabel");
+        Label EffectTimerText = GetNode<Label>("EffectTimerLabel");
 
         ScoreText.Text = "Score: " + Globals.Score;
         HighScoreText.Text = "High Score: " + Globals.HighScore;
+
+        if (Globals.ActiveEffect == "none") {
+            EffectTimerText.Text = "";
+        } else {
+            string EffectTimerType = "";
+            if (Globals.ActiveEffect == "ultra") {
+                EffectTimerType = "Boost";
+            }
+            EffectTimerText.Text = EffectTimerType + " " + "expires in: " + Math.Floor(Globals.EffectTimer * 10) / 10 + "s";
+        }
     }
 }
